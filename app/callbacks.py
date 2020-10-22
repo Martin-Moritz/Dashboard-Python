77569c58ab3a48b63a5d156250ddfe8126449fd0
Martin-Moritz <moritzmartin.pro@gmail.com>
2020-10-22 21:13:48 +0200

    Choix salariés

--- app/callbacks.py
+++ app/callbacks.py
@@ -7,17 +7,21 @@ from .figures import *
 #Callbacks pour rafraichir et mettre à jour les différentes figures
 def register_callbacks(dashapp):
     @dashapp.callback(
-        Output('mapbox', 'figure'),
-        [Input('selection-pays', 'value')])
-    def update_figure(selected_countries):
+        [Output('mapbox', 'figure'), Output('selection-pays', 'disabled')],
+        [Input('selection-pays', 'value'), Input('selection-salarial','value')])
+    def update_figure(selected_countries, selected_salarial):
 
+        #focus de la carte
         focus = 'world'
 
-        filtered_df = df1.loc[df1["SUBJECT"]=="EMPLOYEE"]
+        #Filtrage des données en fonction du choix salariés ou non-salariés
+        if selected_salarial == "SAL":
+            filtered_df = df1.loc[df1["SUBJECT"]=="EMPLOYEE"]
+        else:
+            filtered_df = df1.loc[df1["SUBJECT"]=="SELFEMPLOYED"]
 
-        #Mise à jour de la carte si aucun pays n'a été sélectionné (montre alors tous les pays disponibles)
-        if selected_countries==[]:
-            filtered_df = filtered_df.drop(df1[df1.TIME==1996].index)
+        if selected_salarial != "SAL":
+            filtered_df = filtered_df.drop(filtered_df[filtered_df.LOCATION=="NZL"].index)
             fig1 = px.choropleth(filtered_df, locations="LOCATION", color="Value",
                                                 color_continuous_scale="Jet",
                                                 range_color=(0,50),
@@ -28,41 +32,61 @@ def register_callbacks(dashapp):
                                                 animation_frame="TIME",
                                                 title="Carte",
                                                 height= 700)
-
-        #Mise à jour de la carte en fonction des pays sélectionnés
         else:
-            frames = []
-            for i in selected_countries:
-                frames.append(filtered_df[filtered_df["LOCATION"]==i])
-            filtered_df = pd.concat(frames)
+            #Mise à jour de la carte si aucun pays n'a été sélectionné (montre alors tous les pays disponibles)
+            if selected_countries==[]:
+                filtered_df = filtered_df.drop(filtered_df[filtered_df.TIME==1996].index)
+                fig1 = px.choropleth(filtered_df, locations="LOCATION", color="Value",
+                                                    color_continuous_scale="Jet",
+                                                    range_color=(0,50),
+                                                    scope=focus,
+                                                    labels={"LOCATION":"Pays","TIME":"Année","Value":"Ecart salarial femmes-hommes (%)"},
+                                                    hover_name="PAYS",
+                                                    template = "plotly_dark",
+                                                    animation_frame="TIME",
+                                                    title="Carte",
+                                                    height= 700)
 
-            #Ajuste le focus de la carte si un seul pays est sélectionné
-            if len(selected_countries)==1:
-                if filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Asie":
-                    focus='asia'
-                elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Europe":
-                    focus='europe'
-                elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Amérique du Nord":
-                    focus='north america'
-                elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Amérique du Sud":
-                    focus='south america'
-                elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Afrique":
-                    focus='africa'
+            #Mise à jour de la carte en fonction des pays sélectionnés
+            else:
+                frames = []
+                for i in selected_countries:
+                    frames.append(filtered_df[filtered_df["LOCATION"]==i])
+                filtered_df = pd.concat(frames)
 
-            #Enlève les données qui ne peuvent être comparées (par ex. qu'une seule donnée/pays pour l'année 1975)
-            for i in range(filtered_df["TIME"].min(),filtered_df["TIME"].max()+1):
-                if filtered_df.loc[filtered_df["TIME"]==i].shape[0] != len(frames):
-                    filtered_df = filtered_df.drop(filtered_df[filtered_df.TIME==i].index)
+                #Ajuste le focus de la carte si un seul pays est sélectionné
+                if len(selected_countries)==1:
+                    if filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Asie":
+                        focus='asia'
+                    elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Europe":
+                        focus='europe'
+                    elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Amérique du Nord":
+                        focus='north america'
+                    elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Amérique du Sud":
+                        focus='south america'
+                    elif filtered_df.CONTINENT[filtered_df["LOCATION"]==selected_countries[0]].any()=="Afrique":
+                        focus='africa'
 
-            fig1 = px.choropleth(filtered_df,locations="LOCATION", color="Value",
-                                                color_continuous_scale="Jet",
-                                                range_color=(0,50),
-                                                scope=focus,
-                                                labels={"LOCATION":"Pays","TIME":"Année","Value":"Ecart salarial femmes-hommes (%)"},
-                                                hover_name="PAYS",
-                                                template = "plotly_dark",
-                                                animation_frame="TIME",
-                                                title="Carte",
-                                                height=700)
+                #Enlève les données qui ne peuvent être comparées (par ex. qu'une seule donnée/pays pour l'année 1975)
+                for i in range(filtered_df["TIME"].min(),filtered_df["TIME"].max()+1):
+                    if filtered_df.loc[filtered_df["TIME"]==i].shape[0] != len(frames):
+                        filtered_df = filtered_df.drop(filtered_df[filtered_df.TIME==i].index)
 
-        return fig1
+                fig1 = px.choropleth(filtered_df,locations="LOCATION", color="Value",
+                                                    color_continuous_scale="Jet",
+                                                    range_color=(0,50),
+                                                    scope=focus,
+                                                    labels={"LOCATION":"Pays","TIME":"Année","Value":"Ecart salarial femmes-hommes (%)"},
+                                                    hover_name="PAYS",
+                                                    template = "plotly_dark",
+                                                    animation_frame="TIME",
+                                                    title="Carte",
+                                                    height=700)
+
+        #Activation du menu déroulant
+        if selected_salarial=="SAL":
+            disabled=False
+        else:
+            disabled=True
+
+        return fig1, disabled
--- app/layout.py
+++ app/layout.py
@@ -8,9 +8,11 @@ from .figures import *
 
 #Couleurs utilisées dans le Dashboard
 colors = {
-    'background': '#464A4D',
+    'background': 'white',
     'text': '#49BBEE'
 }
+
+#'background': '#464A4D'
 
 #Disposition des figures et autres composants
 layout = html.Div(style={'backgroundColor': colors['background']}, children=[
@@ -25,14 +27,31 @@ layout = html.Div(style={'backgroundColor': colors['background']}, children=[
 
     #html.Label(["Mapbox", dcc.Dropdown(id="dropdown1")], style={'color':'white','display': 'inline-block', 'width': '20%', 'position':'absolute','top':'20px'}),
 
-    #Menu déroulant pour le choix des pays
-    dcc.Dropdown(
-    id='selection-pays',
-    placeholder='Selectionner un pays',
-    options=options_selection_pays,
-    multi=True,
-    value=["USA","FRA","CAN","KOR","JPN","DEU"]
-    ),
+    html.Div(children=[
+        #Menu déroulant pour le choix des pays
+        dcc.Dropdown(
+        id='selection-pays',
+        placeholder='Selectionner un pays',
+        options=options_selection_pays,
+        multi=True,
+        value=["USA","FRA","CAN","KOR","JPN","DEU"],
+        style={'display': 'inline-block', 'width': '100%'}
+        ),
+    ], style={'display':'inline-block','width':'80%'}),
+
+    html.Div(children=[
+        #Choix entre salariés et non salariés
+        dcc.RadioItems(
+        id='selection-salarial',
+        options=[
+            {'label': 'Salariés', 'value': 'SAL'},
+            {'label': 'Non-salariés', 'value': 'NSAL'}
+        ],
+        value='SAL',
+        labelStyle={'backgroundColor':'white','width':'30%'}
+        ),
+    ], style={'display':'inline-block','width':'30%','position':'absolute','top':'70px','left':'1550px'}),
+
 
     #Figure Mapbox
     html.Div(children=[dcc.Graph(id='mapbox', figure=fig1)]),
