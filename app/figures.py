b8f2cb55a99dc1be7e87ff6db7b2d09b7038f0e5
Martin-Moritz <moritzmartin.pro@gmail.com>
2020-10-26 14:48:31 +0100

    Restructuration figures

--- app/figures.py
+++ app/figures.py
@@ -4,17 +4,11 @@ from datetime import datetime as dt
 
 from .data import *
 
-##Données utilisées qui pourront être modifiées dans les graphiques
-filtered_df = df1
-
-
 ##Figure Carte
 
-#focus de la carte
-focus = 'world'
-
 #structure générale de la carte
-carte_template = px.choropleth(filtered_df,locations="LOCATION", color="Value",
+def carte_template(df,focus='world'):
+    carte = px.choropleth(df,locations="LOCATION", color="Value",
                                     color_continuous_scale="Jet",
                                     range_color=(0,50),
                                     scope=focus,
@@ -24,39 +18,44 @@ carte_template = px.choropleth(filtered_df,locations="LOCATION", color="Value",
                                     animation_frame="TIME",
                                     title="Carte - Écart de revenus liés entre les hommes et les femmes",
                                     height=700)
+    return carte
 
 #fonction pour créer la carte
-def create_carte():
-    carte = carte_template
+def create_carte(df,focus='world'):
+    carte = carte_template(df,focus)
     carte.update_layout(paper_bgcolor='#DCE8FD', coloraxis={"colorbar":{"ticksuffix":"%"}})
     return carte
 
-carte = create_carte()
+carte = create_carte(df1)
 
 
 ##Figure Histogramme
 
 #structure générale de l'histogramme
-histogramme_template = px.histogram(filtered_df, title="Histogramme - Écart de revenus liés entre les hommes et les femmes", x="TIME", y="Value", color="PAYS", labels={'PAYS':'Pays','TIME':'Année', 'Value':"Ecart salarial femmes-hommes"}, template='simple_white', barmode='overlay', opacity=0.5, nbins=26, range_y=(0,60))
+def histogramme_template(df):
+    histogramme = px.histogram(df, title="Histogramme - Écart de revenus liés entre les hommes et les femmes", x="TIME", y="Value", color="PAYS", labels={'PAYS':'Pays','TIME':'Année', 'Value':"Ecart salarial femmes-hommes"}, template='simple_white', barmode='overlay', opacity=0.5, nbins=26, range_y=(0,60))
+    return histogramme
 
 #fonction pour créer l'histogramme
-def create_histogramme():
-    histogramme = histogramme_template
+def create_histogramme(df):
+    histogramme = histogramme_template(df)
     histogramme.update_layout(yaxis={'title':{'text':'Écart salarial femmes-hommes'},"ticksuffix":"%"}, paper_bgcolor='#DCE8FD')
     return histogramme
 
-histogramme = create_histogramme()
+histogramme = create_histogramme(df1)
 
 
 ##Figure Diagramme en barres
 
 #structure générale du diagramme en barres
-diagramme_template = px.histogram(filtered_df, title="Diagramme - Écart de revenus liés entre les hommes et les femmes", x="PAYS", y="Value", color="PAYS", labels={'PAYS':'Pays','TIME':'Année', 'Value':"Ecart salarial femmes-hommes"}, template='simple_white', range_y=(0,60))
+def diagramme_template(df):
+    diagramme = px.histogram(df, title="Diagramme - Écart de revenus liés entre les hommes et les femmes", x="PAYS", y="Value", color="PAYS", labels={'PAYS':'Pays','TIME':'Année', 'Value':"Ecart salarial femmes-hommes"}, template='simple_white', range_y=(0,60))
+    return diagramme
 
 #fonction pour créer le diagramme
-def create_diagramme():
-    diagramme = diagramme_template
+def create_diagramme(df):
+    diagramme = diagramme_template(df)
     diagramme.update_layout(yaxis={'title':{'text':'Écart salarial femmes-hommes'},"ticksuffix":"%"}, paper_bgcolor='#DCE8FD')
     return diagramme
 
-diagramme = create_diagramme()
+diagramme = create_diagramme(df1)
